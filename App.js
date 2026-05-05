f07a8e70156f4fbbf3a2dc7458a63064890b2d04
NKOGallardo <nkogalado@gmail.com>
2026-05-05 18:17:16 +0200

    im to smart

--- App.js
+++ App.js
@@ -1,4 +1,33 @@
 
- // const apiKey = "950302f62873de971a894bf2df4ec176";
-//   const apiKey = "YOUR_API_KEY_HERE";
- // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+async function getWeather() {
+
+    const city = document.getElementById("city").value;
+    const apiKey = "950302f62873de971a894bf2df4ec176";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.cod !== 200) {
+            document.getElementById("result").innerText = data.message;
+            return;
+        }
+
+        const temp = data.main.temp;
+        const weather = data.weather[0].description;
+
+        document.getElementById("result").innerText = `${city}: ${temp}°C, ${weather}`;
+
+    }
+
+    catch(error) {
+
+        document.getElementById("result").innerHTML =
+
+        "Something went wroge"
+
+    }
+
+}
