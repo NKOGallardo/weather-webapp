857d21d5a386a6d125ce2e4c5a5ad31afca66bf2
NKOGallardo <nkogalado@gmail.com>
2026-05-09 19:57:35 +0200

    playing with style in JS

--- App.js
+++ App.js
@@ -2,12 +2,21 @@
 
 async function getWeather() {
 
+    const example = "London";
+
     // display on city
     // apikey from openweathermap
     // communicating with http
+    // When someone did not enter city name, display error message
     const city = document.getElementById("city").value;
     const apiKey = "950302f62873de971a894bf2df4ec176";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const errorNoMessage = innerHTML = 
+        `   
+            <p>Enter city name</p><br>
+            <p>Example: ${example}</p>
+        `
+        ;
 
     try {
 
@@ -15,7 +24,7 @@ async function getWeather() {
         const data = await response.json();
 
         if (data.cod !== 200) {
-            document.getElementById("result").innerText = data.message;
+            document.getElementById("result").innerHTML = errorNoMessage;
             return;
         }
 
@@ -23,7 +32,19 @@ async function getWeather() {
         const temp = data.main.temp;
         const weather = data.weather[0].description;
 
-        document.getElementById("result").innerText = `${city}: ${temp}°C, ${weather}`;
+        document.getElementById("result").innerHTML = 
+            `
+                <h3 class="city">
+                    ${city}
+                </h3>
+                <p class="temp">
+                    ${temp}°C,
+                </p>
+                <p class="weather">
+                    ${weather}
+                </p>
+            `
+        ;
 
     }
 
@@ -31,7 +52,7 @@ async function getWeather() {
 
         document.getElementById("result").innerHTML =
 
-        "Something went wroge"
+        "Something went wrong"
 
     }
 
